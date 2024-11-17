b6414714adcc3194515a04097724a1e14dd65e56
manvithabalne <149790568+manvithabalne@users.noreply.github.com>
2024-11-17 18:46:41 +0530

    Create RPS.py

---
+++ visaa_prep/RPS.py
@@ -0,0 +1,22 @@
+m, n=input().split()
+if m=='R':
+    if n=='P':
+        print("Charan")
+    elif n=='S':
+        print("Vignesh")
+    else:
+        print("NULL")
+elif m=='P':
+    if n=='S':
+        print("Charan")
+    elif n=='R':
+        print("Vignesh")
+    else:
+        print("NULL")
+elif m=='S':
+    if n=='R':
+        print("Charan")
+    elif n=='P':
+        print("Vignesh")
+    else:
+        print("NULL")
