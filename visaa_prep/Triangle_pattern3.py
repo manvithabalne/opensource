462c46fad449a6f6098a69fe45404fa575076095
manvithabalne <149790568+manvithabalne@users.noreply.github.com>
2024-11-17 19:24:29 +0530

    Create Triangle_pattern3.py

---
+++ visaa_prep/Triangle_pattern3.py
@@ -0,0 +1,8 @@
+N=int(input())
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print(" "*(2*(N-i)),end="")
+    for j in range(i,0,-1):
+        print(j,end="")
+    print()
